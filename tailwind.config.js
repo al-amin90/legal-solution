b6b09ca87dbde09852ca33031ab982d72aaa4326
al-amin90 <ijesun30@gmail.com>
2023-09-04 04:50:51 -0700

    FAQ section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,3 @@ module.exports = {
     },
     plugins: [],
   }
-
-  module.exports = {
-    content: [
-      "./src/**/*.{html,js}",
-      "./node_modules/tw-elements/dist/js/**/*.js"
-    ],
-    plugins: [require("tw-elements/dist/plugin.cjs")],
-    darkMode: "class"
-  };
